07d6d0356b65231084a622dec53dcfacc1881a97
Patrick Keenan <patrick.keenan.fyi@gmail.com>
2023-01-22 01:21:58 -0800

    refactor: remove default exports from module files
    deno.land/x/ docs revealed that they are clobbering each other in the root module file. You can't export * without exporting default as well, so the only clean fix is to remove all default exports, which should be cleaner anyway.

--- Day/mod.ts
+++ Day/mod.ts
@@ -2,4 +2,3 @@ export * from "./class.ts";
 export * from "./constants.ts";
 export * from "./functions.ts";
 export * from "./types.ts";
-export { Day as default } from "./class.ts";
--- Duration/mod.ts
+++ Duration/mod.ts
@@ -2,4 +2,3 @@ export * from "./class.ts";
 export * from "./constants.ts";
 export * from "./functions.ts";
 export * from "./types.ts";
-export { Duration as default } from "./class.ts";
